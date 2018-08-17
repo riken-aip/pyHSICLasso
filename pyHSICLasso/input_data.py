782f720321c16cde577f07516451c604b2ff803c
myamada0321 <myamada0321@gmail.com>
2018-08-17 21:11:59 +0900

    Support multi-variate output

--- pyHSICLasso/input_data.py
+++ pyHSICLasso/input_data.py
@@ -10,26 +10,57 @@ from scipy import io as spio
 
 standard_library.install_aliases()
 
-
-def input_csv_file(file_name):
+def input_csv_file(file_name,output_list=['class']):
     df = pd.read_csv(file_name, sep=",")
 
-    #Store the column name (Feature name)
-    featname = df.columns[1:].tolist()
+    featname  = df.columns.tolist()
+    input_index = list(range(0,len(featname)))
+    output_index = []
 
-    X_in = df.ix[:, 1:].values.T
-    Y_in = df.ix[:, 0].values.reshape(1, len(df.index))
+    for output_name in output_list:
+        if output_name in featname:
+            tmp = featname.index(output_name)
+            featname.remove(output_name)
+            output_index.append(tmp)
+            input_index.remove(tmp)
+        else:
+            raise ValueError("Output variable, %s, not found" % (output_name))
+
+
+    X_in = df.ix[:, input_index].values.T
+
+    if len(output_index) == 1:
+        Y_in = df.ix[:, output_index].values.reshape(1, len(df.index))
+    else:
+        Y_in = df.ix[:, output_index].values.T
+
     return X_in, Y_in, featname
 
 
-def input_tsv_file(file_name):
+def input_tsv_file(file_name,output_list=['class']):
     df = pd.read_csv(file_name, sep="\t")
 
     # Store the column name (Feature name)
-    featname = df.columns[1:].tolist()
+    featname = df.columns.tolist()
+    input_index = list(range(0, len(featname)))
+    output_index = []
 
-    X_in = df.ix[:, 1:].values.T
-    Y_in = df.ix[:, 0].values.reshape(1, len(df.index))
+    for output_name in output_list:
+        if output_name in featname:
+            tmp = featname.index(output_name)
+            featname.remove(output_name)
+            output_index.append(tmp)
+            input_index.remove(tmp)
+        else:
+            raise ValueError("Output variable, %s, not found" % (output_name))
+
+    X_in = df.ix[:, input_index].values.T
+
+    if len(output_index) == 1:
+        Y_in = df.ix[:, output_index].values.reshape(1, len(df.index))
+    else:
+        Y_in = df.ix[:, output_index].values.T
+
     return X_in, Y_in, featname
 
 
