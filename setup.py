50c4b1453b9b7a43a748796fba4aea51578bd19d
Héctor Climente-González <hclimente@gmail.com>
2018-12-01 13:41:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def _packages():
 
 setup(
     name="pyHSICLasso",
-    version="1.3.3",
+    version="1.3.4",
     author="Makoto Yamada",
     author_email="makoto.yamada@riken.jp",
     url="http://www.makotoyamada-ml.com/hsiclasso.html",
