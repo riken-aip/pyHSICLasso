cea4cfe42a8ed2e9c564c320aac76444231058fa
hclimente <hclimente@gmail.com>
2019-04-05 18:32:40 +0200

    version bump

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def _packages():
 
 setup(
     name="pyHSICLasso",
-    version="1.4.1",
+    version="1.4.2",
     author="Makoto Yamada",
     author_email="makoto.yamada@riken.jp",
     url="http://www.makotoyamada-ml.com/hsiclasso.html",
